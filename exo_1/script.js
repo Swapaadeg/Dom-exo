cfa5c4344f9923cf9cafa1fa73f5d503283e1aa9
Marie Rivier <marie.rivier23@gmail.com>
2025-04-10 12:11:50 +0200

    first commit

---
+++ exo_1/script.js
@@ -0,0 +1,9 @@
+const input = document.createElement('input')
+input.style.display = 'block'
+input.placeholder = 'Tapez quelque chose'
+document.body.append(input)
+
+const button = document.createElement('button')
+button.textContent = 'Cliquez ici'
+document.body.append(button)
+input.addEventListener()
