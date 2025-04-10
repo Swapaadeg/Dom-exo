68d79cedc8513a419e900e7e27f4e14318b98ecd
Marie Rivier <marie.rivier23@gmail.com>
2025-04-10 13:25:55 +0200

    exo1

--- exo_1/script.js
+++ exo_1/script.js
@@ -1,9 +1,30 @@
 const input = document.createElement('input')
-input.style.display = 'block'
-input.placeholder = 'Tapez quelque chose'
+input.id = 'userInput'
+input.placeholder = 'Tapez quelque chose...'
 document.body.append(input)
 
+const paragraph = document.createElement('p')
+paragraph.id = "paragraph"
+paragraph.textContent = ''
+document.body.append(paragraph)
+
 const button = document.createElement('button')
-button.textContent = 'Cliquez ici'
+button.textContent = 'Valider'
 document.body.append(button)
-input.addEventListener()
+
+
+button.addEventListener('click', function() {
+    const userInput =input.value
+    if (userInput === 'Bonjour'){
+        paragraph.textContent = 'Bonne journée'
+        paragraph.style.color = 'green'
+  } else if (userInput === 'Au revoir') {
+        paragraph.textContent = 'A la prochaine!'
+        paragraph.style.color = 'green'
+  }else {
+    paragraph.textContent = 'Je ne comprends pas'
+    paragraph.style.color = 'red'
+  }
+
+ })
+
