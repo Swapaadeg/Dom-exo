c30c39631cba847fc6413c87e0aabc3907957fec
Marie Rivier <marie.rivier23@gmail.com>
2025-04-10 14:03:53 +0200

    exo2

---
+++ exo_2/script.js
@@ -0,0 +1,27 @@
+const input1 = document.createElement('input')
+input1.type = 'number'
+input1.id = 'number1'
+input1.style.marginRight = '10rem'
+input1.style.width = '40%'
+document.body.append(input1)
+
+const input2 = document.createElement('input')
+input2.type = 'number'
+input2.style.width = '40%'
+input2.id = 'number2'
+document.body.append(input2)
+
+const paragraph = document.createElement('p')
+paragraph.id = 'result'
+paragraph.textContent = 'Résultat : '
+document.body.append(paragraph)
+
+const button = document.createElement('button')
+button.textContent = 'Calculer'
+document.body.append(button)
+
+button.addEventListener('click', function() {
+    const number1 = parseFloat(input1.value) || 0
+    const number2 = parseFloat(input2.value) || 0
+    result.textContent = 'Résultat : ' + (number1 + number2)
+})
