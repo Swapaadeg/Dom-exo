ba5ed42d5008c6e068ce8d5872250968249af6ce
Marie Rivier <marie.rivier23@gmail.com>
2025-04-11 14:33:26 +0200

    ajout des boutons

--- z-Liste de tâches/script.js
+++ z-Liste de tâches/script.js
@@ -1,11 +1,43 @@
 function addTask() {
     const input = document.getElementById("task-input");
        const taskText = input.value.trim()
+
     if (taskText !== "") {
-        const li = document.createElement("li");
+        const li = document.createElement('li');
         li.textContent = taskText;
+
+        const endBtn = document.createElement('button')
+        endBtn.textContent = 'Terminé'
+        endBtn.id = 'endBtn'
+        endBtn.style.backgroundColor = 'green'
+        endBtn.onclick = function() {
+            moveToCompleted(li)
+        }
+
+        const supprBtn = document.createElement('button')
+        supprBtn.textContent = 'Supprimer'
+        supprBtn.id = 'supprBtn'
+        supprBtn.style.backgroundColor = 'red'
+        endBtn.onclick = function() {
+            li.remove()
+        }
+
+        const editBtn = document.createElement('button')
+        editBtn.textContent = 'Modifier'
+        editBtn.id = 'editBtn'
+        editBtn.style.backgroundColor = 'yellow'
+        editBtn.style.color = 'black'
+        editBtn.onclick = function() {
+            editTask(span, editBtn)
+        }
+
+        li.appendChild(endBtn)
+        li.appendChild(supprBtn)
+        li.appendChild(editBtn)
+
         const taskList = document.getElementById("task-list");
         taskList.appendChild(li);
         input.value = "";
     }
 }
+
